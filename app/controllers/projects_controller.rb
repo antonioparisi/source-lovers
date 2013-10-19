293ac28213b390d6fd71e2ff88bbf419ff11cbd3
Matteo Centenaro <bugant@gmail.com>
2013-10-19 19:46:11 +0200

    Paginate GET /projects
    GET /projects is now paginated to easy navigate through projects

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -4,7 +4,8 @@ class ProjectsController < ApplicationController
   # GET /projects
   # GET /projects.json
   def index
-    @projects = params[:q] ? Project.search(params[:q]) : Project.all
+    @projects = params[:q] ? Project.search(params[:q]) : Project
+    @projects = @projects.page((params[:page] || 1).to_i)
   end
 
   # GET /projects/1
@@ -13,13 +14,8 @@ class ProjectsController < ApplicationController
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_project
-      @project = Project.find(params[:id])
-    end
 
-    # Never trust parameters from the scary internet, only allow the white list through.
-    def project_params
-      params.require(:project).permit(:name, :description, :version)
-    end
+  def set_project
+    @project = Project.find(params[:id])
+  end
 end
