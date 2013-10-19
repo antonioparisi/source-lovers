c1af228265fcd70588cc869bc40deec2ec17a333
Matteo Centenaro <bugant@gmail.com>
2013-10-19 19:19:56 +0200

    Add search param to ProjectsController

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -4,7 +4,7 @@ class ProjectsController < ApplicationController
   # GET /projects
   # GET /projects.json
   def index
-    @projects = Project.all
+    @projects = params[:q] ? Project.search(params[:q]) : Project.all
   end
 
   # GET /projects/1
