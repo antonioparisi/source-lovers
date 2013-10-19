7831a198fe4346bceba49acc1e8e941d1f3371e8
Matteo Centenaro <bugant@gmail.com>
2013-10-19 17:01:43 +0200

    Add fulltext search to Projects
    Add full-text search to Projects + fix hstore issue

--- spec/models/project_spec.rb
+++ spec/models/project_spec.rb
@@ -1,5 +1,50 @@
 require 'spec_helper'
 
 describe Project do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'validations' do
+    [:name, :description, :languages, :author].each do |attr|
+      it { should validate_presence_of(attr) }
+    end
+  end
+
+  describe '.search' do
+    before do
+      @cool = create(:project, :name => 'cool', :description => 'a cool project', :languages => 'ruby', :author => 'good fellow')
+      @oldish = create(:project, :name => 'camel', :description => 'just an old regexp project', :languages => 'perl', :author => 'anonymous@example.com')
+      @old_and_good = create(:project, :name =>'old and good', :description => 'only for real men', :languages => 'C', :author => 'hacker@example.com')
+      @parallel = create(:project, :name => 'parallel', :description => 'we love concurrency and parallel things for good!', :languages => 'erlang', :author => 'master@example.com')
+    end
+
+    context 'when searching for old' do
+      it 'finds two projects' do
+        Project.search('old').count.should == 2
+      end
+
+      it 'finds the oldish project' do
+        Project.search('old').should include(@oldish)
+      end
+
+      it 'finds the old_and_good project' do
+        Project.search('old').should include(@old_and_good)
+      end
+    end
+
+    context 'when searching for good' do
+      it 'finds three projects' do
+        Project.search('good').count.should == 3
+      end
+
+      it 'finds the cool project' do
+        Project.search('good').should include(@cool)
+      end
+
+      it 'finds the old_and_good project' do
+        Project.search('good').should include(@old_and_good)
+      end
+
+      it 'finds the parallel project' do
+        Project.search('good').should include(@parallel)
+      end
+    end
+  end
 end
