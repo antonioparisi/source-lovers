b1e28f006420f78f8449fd4c1f997868c76733a2
Giovanni Cappellotto <potomak84@gmail.com>
2013-10-19 09:32:41 +0200

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+SourceLovers::Application.config.secret_key_base = 'fd7f184aa5373356bbf82e5a51425908bfd350512d675b23ee98efdf028f7b5505aca381030a9e22ef5769e67fc1b309cd7168ed614b980972440758918e0444'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+SourceLovers::Application.config.session_store :cookie_store, key: '_source_lovers_session'
