7e8ffc8b3213e99d195f69f01eb2f90f29154ab8
Giovanni Cappellotto <potomak84@gmail.com>
2013-10-19 10:44:33 +0200

    Add welcome page controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,12 @@
 SourceLovers::Application.routes.draw do
+
   resources :projects, :only => [:index, :show]
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  # root 'welcome#index'
+  root 'welcome#index'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
