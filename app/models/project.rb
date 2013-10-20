198015f30ef9f855f5517586e98f14669b47cf82
Giovanni Cappellotto <potomak84@gmail.com>
2013-10-20 12:24:53 +0200

    Update project style; add homepage property

--- app/models/project.rb
+++ app/models/project.rb
@@ -12,4 +12,8 @@ class Project < ActiveRecord::Base
   def paypal_email
     data['paypal_email'] if data && data['paypal_email']
   end
+
+  def homepage
+    data['homepage'] if data && data['homepage']
+  end
 end
