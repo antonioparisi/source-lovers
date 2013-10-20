0602151efe021d816b8efc00d69b6b0716f0682e
Giovanni Cappellotto <potomak84@gmail.com>
2013-10-20 12:47:42 +0200

    Remove JSON views

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -2,14 +2,12 @@ class ProjectsController < ApplicationController
   before_action :set_project, only: [:show]
 
   # GET /projects
-  # GET /projects.json
   def index
     @projects = params[:q] ? Project.search(params[:q]) : Project
     @projects = @projects.page((params[:page] || 1).to_i)
   end
 
   # GET /projects/1
-  # GET /projects/1.json
   def show
   end
 
