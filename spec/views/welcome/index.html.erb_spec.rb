af28308f078bf329cd3b3112e77c4628f1952d7a
Giovanni Cappellotto <potomak84@gmail.com>
2013-10-20 12:43:13 +0200

    Fix index spec

--- spec/views/welcome/index.html.erb_spec.rb
+++ spec/views/welcome/index.html.erb_spec.rb
@@ -4,6 +4,6 @@ describe "welcome/index.html.erb" do
   it "renders a welcome page" do
     render
 
-    assert_select "h1", :text => "Welcome#index"
+    assert_select "h1", :text => "We are Source Lovers"
   end
 end
